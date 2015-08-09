d2f54aa0a8c1c08b49796e2cf0dba18db7ef9b7b
Sergey Zagoruyko <zagoruyko2@gmail.com>
2015-08-09 16:28:04 +0200

    add gradInput and accGradParams

--- SpatialConvolutionRTC.lua
+++ SpatialConvolutionRTC.lua
@@ -4,6 +4,10 @@ function SpatialConvolution:__init(nInputPlane, nOutputPlane, kW, kH, dW, dH, pa
   parent.__init(self,nInputPlane,nOutputPlane,kW,kH,dW,dH,padW,padH)
 end
 
+function SpatialConvolution:getIm2ColParams()
+  return self.kW, self.kH, self.dW, self.dH, self.padW, self.padH
+end
+
 function SpatialConvolution:updateOutput(input)
   assert(input:nDimension() == 4)
   
@@ -24,9 +28,51 @@ function SpatialConvolution:updateOutput(input)
 
   for i=1,batchSize do
     self.output[i]:t():mm(ones,self.bias:view(1,self.nOutputPlane))
-    columns.nn.im2col(columns, input[i], self.kW, self.kH, self.dW, self.dH, self.padW, self.padH)
+    columns.nn.im2col(columns, input[i], self:getIm2ColParams())
     self.output[i]:addmm(self.weight, columns)
   end
 
   self.output = self.output:view(batchSize, self.nOutputPlane, outputHeight, outputWidth)
+  return self.output
 end
+
+function SpatialConvolution:updateGradInput(input,gradOutput)
+  assert(input:nDimension() == 4)
+  assert(gradOutput:nDimension() == 4)
+
+  self.gradInput:resizeAs(input)
+
+  local columns = self.finput
+  for i=1,input:size(1) do
+    columns:mm(self.weight:t(), gradOutput[i]:view(self.nOutputPlane,-1))
+    columns.nn.col2im(self.gradInput[i], columns, self:getIm2ColParams())
+  end
+
+  return self.gradInput
+end
+
+function SpatialConvolution:accGradParameters(input, gradOutput, scale)
+  assert(input:nDimension() == 4)
+  assert(gradOutput:nDimension() == 4)
+
+  local scale = scale or 1
+
+  local inputWidth   = input:size(4)
+  local inputHeight  = input:size(3)
+  local outputWidth  = (inputWidth + 2*self.padW - self.kW) / self.dW + 1
+  local outputHeight = (inputHeight + 2*self.padH - self.kH) / self.dH + 1
+
+  local ones = self.fgradInput
+  if ones:nDimension() ~= 2 or ones:size(1)*ones:size(2) ~= outputHeight*outputWidth then
+    ones:resize(outputHeight * outputWidth,1):fill(1)
+  end
+
+  local columns = self.finput
+  for i=1,input:size(1) do
+    local go = gradOutput[i]:view(self.nOutputPlane,-1)
+    columns.nn.im2col(columns, input[i], self:getIm2ColParams())
+    self.gradWeight:addmm(scale, go, columns:t())
+    self.gradBias:addmv(scale, go, ones:view(-1)) 
+  end
+end
+
--- im2col.lua
+++ im2col.lua
@@ -1,4 +1,7 @@
 local templet = require 'templet'
+local nvrtc = require 'nvrtc'
+
+local cache = {im2col = {}, col2im = {}}
 
 local im2col_src = templet.loadstring[[
 #define CUDA_KERNEL_LOOP(i, n)                        \
@@ -20,6 +23,7 @@ __global__ void im2col_kernel(const float* data_im, float* data_col) {
     int w_in = w_out * ${stride_w} - ${pad_w};
     data_col += (channel_out * ${height_col} + h_out) * ${width_col} + w_out;
     data_im += (channel_in * ${height} + h_in) * ${width} + w_in;
+    #pragma unroll
     for (int i = 0; i < ${ksize_h}; ++i) {
       for (int j = 0; j < ${ksize_w}; ++j) {
         int h = h_in + i;
@@ -43,27 +47,20 @@ extern "C"
 __global__ void col2im_kernel(const float* data_col, float* data_im) {
   CUDA_KERNEL_LOOP(index, ${n}) {
     float val = 0;
-    int w = index % {width} + {pad_w};
-    int h = (index / {width}) % {height} + {pad_h};
-    int c = index / ({width} * {height});
+    int w = index % ${width} + ${pad_w};
+    int h = (index / ${width}) % ${height} + ${pad_h};
+    int c = index / (${width} * ${height});
     // compute the start and end of the output
-    int w_col_start = (w < {patch_w}) ? 0 : (w - patch_w) / {stride_w} + 1;
-    int w_col_end = min(w / {stride_w} + 1, {width_col});
-    int h_col_start = (h < {patch_h}) ? 0 : (h - patch_h) / {stride_h} + 1;
-    int h_col_end = min(h / {stride_h} + 1, {height_col});
-    /*
-       for (int h_col = h_col_start; h_col < h_col_end; ++h_col) {
-       for (int w_col = w_col_start; w_col < w_col_end; ++w_col) {
-    // the col location: [c * {width} * {height} + h_out, w_out]
-    int c_col = c * {patch_h} * {patch_w} + (h - h_col * {stride_h}) * ksize + (w - w_col * {stride_w});
-    val += data_col[(c_col * {height_col} + h_col) * {width_col} + w_col];
-    }
-    }
-     */
+    int w_col_start = (w < ${patch_w}) ? 0 : (w - ${patch_w}) / ${stride_w} + 1;
+    int w_col_end = min(w / ${stride_w} + 1, ${width_col});
+    int h_col_start = (h < ${patch_h}) ? 0 : (h - ${patch_h}) / ${stride_h} + 1;
+    int h_col_end = min(h / ${stride_h} + 1, ${height_col});
+
     // equivalent implementation
-    int offset = (c * {patch_h} * {patch_w} + h * patch_w + w) * {height_col} * {width_col};
-    int coeff_h_col = (1 - {stride_h} * {patch_w} * {height_col}) * {width_col};
-    int coeff_w_col = (1 - {stride_w} * {height_col} * {width_col});
+    int offset = (c * ${patch_h} * ${patch_w} + h * ${patch_w} + w) * ${height_col} * ${width_col};
+    int coeff_h_col = (1 - ${stride_h} * ${patch_w} * ${height_col}) * ${width_col};
+    int coeff_w_col = (1 - ${stride_w} * ${height_col} * ${width_col});
+    #pragma unroll
     for (int h_col = h_col_start; h_col < h_col_end; ++h_col) {
       for (int w_col = w_col_start; w_col < w_col_end; ++w_col) {
         val += data_col[offset + h_col * coeff_h_col + w_col * coeff_w_col];
@@ -73,6 +70,14 @@ __global__ void col2im_kernel(const float* data_col, float* data_im) {
   }
 }
 ]]
+
+local function hash(t)
+  local s = ''
+  for i,v in pairs(t) do
+    s = ('%s,%d'):format(s,v)
+  end
+  return s
+end
 
 local CUDA_NUM_THREADS = 1024
 
@@ -91,7 +96,7 @@ function torch.CudaTensor.nn.im2col(dst,src,kW,kH,dW,dH,padW,padH)
 
   dst:resize(nInputPlane*kW*kH, height_col*width_col)
 
-  local kernel = im2col_src{
+  local options = {
     height_col = height_col,
     width_col = width_col,
     n = n,
@@ -106,30 +111,38 @@ function torch.CudaTensor.nn.im2col(dst,src,kW,kH,dW,dH,padW,padH)
     channels = nInputPlane
   }
 
-  -- TODO: cache
-  im2col_ptx = im2col_ptx or nvrtc.compileReturnPTX(kernel)
+  local str = hash(options)
+  local ptx = cache.im2col[str]
+  if not ptx then
+    local kernel = im2col_src(options)
+    ptx = nvrtc.compileReturnPTX(kernel)
+    cache.im2col[str] = ptx
+  end
   cutorch.launchPTX(ptx, 'im2col_kernel', {src, dst}, {GET_BLOCKS(n)}, {CUDA_NUM_THREADS})
 end
 
 function torch.CudaTensor.nn.col2im(dst,src,kW,kH,dW,dH,padW,padH)
-  assert(src:nDimension() == 3)
-  local height = src:size(2)
-  local width = src:size(3)
-  local nInputPlane = src:size(1)
+  assert(src:nDimension() == 2)
+  assert(dst:nDimension() == 3)
+  local height = dst:size(2)
+  local width = dst:size(3)
+  local nInputPlane = dst:size(1)
   local height_col = (height + 2 * padH - kH) / dH + 1
   local width_col = (width + 2 * padW - kW) / dW + 1
-  local n = nInputPlane * height_col * width_col
+  local n = nInputPlane * height * width
 
-  dst:resize(nInputPlane*kW*kH, height_col*width_col)
+  --dst:resize(nInputPlane*kW*kH, height_col*width_col)
+  assert(src:size(1) == nInputPlane*kW*kH)
+  assert(src:size(2) == height_col*width_col)
 
-  local kernel = col2im_src{
+  local options = {
     height_col = height_col,
     width_col = width_col,
     n = n,
     height = height,
     width = width,
-    ksize_h = kH,
-    ksize_w = kW,
+    patch_h = kH,
+    patch_w = kW,
     pad_h = padH,
     pad_w = padW,
     stride_h = dH,
@@ -137,7 +150,12 @@ function torch.CudaTensor.nn.col2im(dst,src,kW,kH,dW,dH,padW,padH)
     channels = nInputPlane
   }
 
-  -- TODO: cache
-  col2im_ptx = col2im_ptx or nvrtc.compileReturnPTX(kernel)
+  local str = hash(options)
+  local ptx = cache.col2im[str]
+  if not ptx then
+    local kernel = col2im_src(options)
+    ptx = nvrtc.compileReturnPTX(kernel)
+    cache.col2im[str] = ptx
+  end
   cutorch.launchPTX(ptx, 'col2im_kernel', {src, dst}, {GET_BLOCKS(n)}, {CUDA_NUM_THREADS})
 end
